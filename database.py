9b982ce746747186e1d4ce27a91e48eda5517853
Kirtan Jain <94744255+kirtann@users.noreply.github.com>
2023-05-01 08:58:02 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+# SocioAI
 import pymysql as p
 
 
@@ -15,9 +16,9 @@ def connectdb():
         l='CREATE TABLE IF NOT EXISTS NormalUser(u_id int(10),name varchar(30),password varchar(20),ph_no int(10),fav_query_pointer int(10))'
         k='CREATE TABLE IF NOT EXISTS ProUser(u_id int(10),name varchar(30),password varchar(20),ph_no int(10),fav_query_pointer int(10),alter_query varchar(20),alter_solution varchar(20))'
         m='CREATE TABLE IF NOT EXISTS Adminpermit(u_id int(10),name varchar(30),password varchar(20),ph_no int(10),fav_query_pointer int(10),alter_query varchar(20),alter_solution varchar(20))'
-        n='CREATE TABLE IF NOT EXISTS AddQuery(alter_query varchar(100),alter_solution varchar(50))'
-        b='CREATE TABLE IF NOT EXISTS QueryHospital(keyword varchar(10),solution varchar(10), query_pointer int(5))'
-        j='CREATE TABLE IF NOT EXISTS QueryEducation(keyword varchar(10),solution varchar(10), query_pointer int(5))'
+        n='CREATE TABLE IF NOT EXISTS AddQuery(id int(2) NOT NULL AUTO_INCREMENT, alter_query varchar(100),alter_solution varchar(50), PRIMARY KEY (id))'
+        b='CREATE TABLE IF NOT EXISTS QueryHospital(disease varchar(50),medicine varchar(50), query_pointer int(3))'
+        j='CREATE TABLE IF NOT EXISTS QueryEducation(name varchar(50), avg_package int(10), nirf_ranking int(3), criteria varchar(40), query_pointer int(3))'
         cur.execute(l)
         cur.execute(k)
         cur.execute(m)

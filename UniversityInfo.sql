4dd8d0fa9901f059cf59690ef128978d07a7b82d
Kirtan Jain <94744255+kirtann@users.noreply.github.com>
2023-05-11 22:41:19 +0530

    Create UniversityInfo.sql

---
+++ UniversityInfo.sql
@@ -0,0 +1,12 @@
+INSERT INTO `queryeducation` (`name`, `avg_package`, `nirf_ranking`, `criteria`, `query_pointer`) VALUES
+('lovely professional university', 160000, '51', 'lpunest', 0),
+('jadavpur university', 1600000, '11', 'jee mains', 0),
+('chandigarh university', 200000, '45', 'boards', 0),
+('nit tiruchirappalli', 1000000, '8', 'jee advanced', 0),
+('iit bhu', 7600000, '6', 'lpunest', 0),
+('banasthali vidyapith', 460000, '59', 'entrance', 0),
+('thapar institute of engineering', 200000, '28', 'entrance', 0),
+('delhi technological university', 2600000, '35', 'jee mains', 0),
+('aligarh muslim university', 150000, '37', 'jee mains', 0),
+('amrita vishwa vidyapeetham', 1600000, '47', 'entrance', 0),
+('institute of chemical technology', 1200000, '18', 'mh-cet', 0);
